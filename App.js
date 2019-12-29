9d4df09dae5a287c6acd9bdfd6bea333e7cd7b29
leejeongmin <danmin20@khu.ac.kr>
2019-12-29 19:15:52 +0900

    UI

--- App.js
+++ App.js
@@ -1,19 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform
+} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle="light-content"/>
+      <Text style={styles.title}>Must To Do</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.newToDo} placeholder={"New To Do"}/>
+      </View>
     </View>
   );
 }
 
+const { height, width } = Dimensions.get("window");
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#C1C2FF",
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+    color: "white",
+    fontSize: 20,
+    marginTop: 50,
+    fontStyle: "normal",
+    fontWeight: "200",
+    marginBottom: 30,
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios:{
+        shadowColor: "rgb(50,50,50)",
+        shadowOpacity: 0,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  }
 });
